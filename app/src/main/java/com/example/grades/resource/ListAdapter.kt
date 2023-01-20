0b84f649ec3e22b24e36f31f34fd2f871b10de19
LeivasGabi <leivasgabi@gmail.com>
2023-01-20 15:37:31 -0300

    adapter

--- app/src/main/java/com/example/grades/resource/ListAdapter.kt
+++ app/src/main/java/com/example/grades/resource/ListAdapter.kt
@@ -1,4 +1,22 @@
 package com.example.grades.resource
 
-class ListAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.grades.model.Students
+
+class ListAdapter (private val onClicked: () -> Unit): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private lateinit var items: List<Students>
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 }
--- app/src/main/java/com/example/grades/view/RegisterFragment.kt
+++ app/src/main/java/com/example/grades/view/RegisterFragment.kt
@@ -5,10 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.grades.R
+import com.example.grades.model.Grades
+import com.example.grades.model.Students
+import kotlinx.android.synthetic.main.fragment_register.*
 
 class RegisterFragment : Fragment(R.layout.fragment_register) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        newStudent()
+    }
+    private fun newStudent() {
+        val name = editTextStudentName.toString()
+        val subject = editTextSubject.toString()
+        val gradeOne = editTextGradeFirst.toString().toFloat()
+        val gradeTwo = editTextGradeSecond.toString().toFloat()
+        val gradeThree = editTextGradeThird.toString().toFloat()
+        val gradeFour = editTextGradeFourth.toString().toFloat()
+
+        val student = Students(
+            name = name,
+            Grades(
+                subject,
+                gradeOne,
+                gradeTwo,
+                gradeThree,
+                gradeFour
+            )
+        )
+        val action = RegisterFragmentDirections.actionRegisterFragmentToHomeFragment2(
+            student
+        )
+        findNavController().navigate(action)
     }
 }
